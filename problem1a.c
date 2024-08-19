c78c31732d2caedc7179ffa04e8c5b55bcda4bfb
SHAIK-MOHAMMAD-IRFAN273 <ShaikMohammad.Irfan@iiitb.ac.in>
2024-08-19 18:39:04 +0530

    irfan

---
+++ problem1a.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(int nums,char *files[]){
+if(nums!=3){
+fprintf(stderr,"no sufficeint files ");
+return 1;
+}
+const char *target=files[1];
+const char *link=files[2];
+if(symlink(target,link)==0){
+printf("soft link created between %s and %s",target,link);
+return 0;
+}
+else{
+perror("symlink");
+return -1;
+}
+}
